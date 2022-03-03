34bd1377f51ee89559e39460404ea102d725f5ff
Jameson Chen <[jamesonychan@gmail.com]>
2022-03-03 02:09:53 -0500

    frontend commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
-
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HomeScreen from "./components/HomeScreen";
+import ProductScreen from "./components/ProductScreen";
 
 function App() {
   return (
-    <>
-      <h1>hello</h1>
-    </>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
