466742620abd15f366a0e001d17a18f515763619
Jameson Chen <[jamesonychan@gmail.com]>
2022-03-02 23:26:46 -0500

    setup commit

---
+++ frontend/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface HeaderProps {
+
+}
+
+const Header: React.FC<HeaderProps> = ({}) => {
+        return (
+            <h1>Header</h1>
+        );
+}
+
+export default Header
