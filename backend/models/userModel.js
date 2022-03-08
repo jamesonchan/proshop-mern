79475dd0af4bc945a3ff311a5b1bccfb268cdf3c
Jameson Chen <[jamesonychan@gmail.com]>
2022-03-08 03:56:34 -0500

    backend user routes commit

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const { Schema } = mongoose;
 
@@ -27,6 +28,20 @@ const userSchema = Schema(
     timestamps: true,
   }
 );
+
+// compare bcrypt password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// middleware of create password
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model("User", userSchema);
 
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import colors from "colors";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
@@ -11,11 +12,15 @@ connectDB();
 
 const app = express();
 
+// access to json body
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 
